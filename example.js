63646f132222fbe2e25c5206c7fb87f6644910ca
Nimo <nimo.jser@gmail.com>
2018-02-09 17:30:48 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 var express = require('express')
-var echoMock = require('./lib/index')
+var echoMock = require('echo-mock')
 var app = express()
 app.use(echoMock({
     dbFile: __dirname + '/db.json'
 }))
-var port = 3000
+var port = 9823
 app.listen(port, function () {
     console.log('http://127.0.0.1:' + port)
 })
